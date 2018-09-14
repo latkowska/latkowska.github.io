eb93804b1a77288be8ba5a8243bd7834d0db8459
latkowska <latkowska.p@gmail.com>
2018-09-14 10:15:12 +0200

    localhost -> heroku

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://guarded-taiga-74762.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://guarded-taiga-74762.herokuapp.com/v1/task/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
