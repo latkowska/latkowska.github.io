d8314c263523307fd428d8d89fff69c91154307a
latkowska <latkowska.p@gmail.com>
2018-09-17 22:27:43 +0200

    roots updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://guarded-taiga-74762.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://guarded-taiga-74762.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://guarded-taiga-74762.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
